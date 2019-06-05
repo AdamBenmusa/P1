8b56c6a8695045876bfdfbd71ef3ad743f74d35c
AdamBenmusa <51390611+AdamBenmusa@users.noreply.github.com>
2019-06-04 20:58:52 -0500

    Add files via upload

---
+++ Hi_LC.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Hi_LC.cpp
+// Author      : Adam
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello Launch Code" << endl; // prints Hello Launch Code
+	return 0;
+}
